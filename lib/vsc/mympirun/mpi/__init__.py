0d88340a43f54c1f8037abf8e69d3f5dee5e8cfd
jerodcle <jeroen.declerck@ugent.be>
2016-07-28 12:16:49 +0200

    removed init import

--- lib/vsc/mympirun/mpi/__init__.py
+++ lib/vsc/mympirun/mpi/__init__.py
@@ -25,5 +25,5 @@
 """
 Allow other packages to extend this namespace, zip safe setuptools style
 """
-import pkg_resources
-pkg_resources.declare_namespace(__name__)
+#import pkg_resources
+#pkg_resources.declare_namespace(__name__)
